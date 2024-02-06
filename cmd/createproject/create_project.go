bdaed7ba1b658fb20f9daae2c7124b04b8830e47
Hidayat Hamir <hidayat.03@erajaya.com>
2024-02-06 10:11:01 +0700

    fix: add log

--- cmd/createproject/create_project.go
+++ cmd/createproject/create_project.go
@@ -12,44 +12,58 @@ import (
 
 // CreateProject is main func to create new project.
 func CreateProject(projectName string, moduleName string) {
+	log.Println("create project start")
+
+	log.Println("create project dir")
 	projectPath := filepath.Join(".", projectName)
 	if err := os.MkdirAll(projectPath, os.ModePerm); err != nil {
 		log.Fatal(fmt.Errorf("err mkdir projectPath: %v", err))
 	}
 
+	log.Println("run go mod init in project dir")
 	if err := runGoModInit(moduleName, projectPath); err != nil {
 		log.Fatal(fmt.Errorf("err run go mod init in project path: %v", err))
 	}
 
+	log.Println("create internal/controller dir in project dir")
 	if err := os.MkdirAll(filepath.Join(projectPath, "internal", "controller"), os.ModePerm); err != nil {
 		log.Fatal(fmt.Errorf("err mkdir projectPath/internal/controller: %v", err))
 	}
 
+	log.Println("create internal/service dir in project dir")
 	if err := os.MkdirAll(filepath.Join(projectPath, "internal", "service"), os.ModePerm); err != nil {
 		log.Fatal(fmt.Errorf("err mkdir projectPath/internal/service: %v", err))
 	}
 
+	log.Println("create internal/repository dir in project dir")
 	if err := os.MkdirAll(filepath.Join(projectPath, "internal", "repository"), os.ModePerm); err != nil {
 		log.Fatal(fmt.Errorf("err mkdir projectPath/internal/repository: %v", err))
 	}
 
+	log.Println("create cmd dir in project dir")
 	if err := os.MkdirAll(filepath.Join(projectPath, "cmd"), os.ModePerm); err != nil {
 		log.Fatal(fmt.Errorf("err mkdir projectPath/cmd: %v", err))
 	}
 
+	log.Println("create main.go file in cmd dir")
 	if err := os.WriteFile(filepath.Join(projectPath, "cmd", "main.go"), []byte("package main\n"), os.ModePerm); err != nil {
 		log.Fatal(fmt.Errorf("err write file projectPath/cmd/main.go: %v", err))
 	}
 
+	log.Println("change current dir to project dir")
 	if err := os.Chdir(projectPath); err != nil {
 		log.Fatal(fmt.Errorf("err change work dir to projectPath: %v", err))
 	}
 
+	log.Println("create domain hello world")
 	createdomain.CreateDomain("hello world")
 
+	log.Println("run go mod tidy")
 	if err := runGoModTidy(); err != nil {
 		log.Fatal(fmt.Errorf("err run go mod tidy in projectPath: %v", err))
 	}
+
+	log.Println("create project finish, go to your project:\n\tcd", projectPath)
 }
 
 func runGoModInit(moduleName string, projectPath string) error {
@@ -60,7 +74,10 @@ func runGoModInit(moduleName string, projectPath string) error {
 		return err
 	}
 
-	fmt.Println(string(stdout))
+	if string(stdout) != "" {
+		fmt.Println(string(stdout))
+	}
+
 	return nil
 }
 
@@ -71,6 +88,9 @@ func runGoModTidy() error {
 		return err
 	}
 
-	fmt.Println(string(stdout))
+	if string(stdout) != "" {
+		fmt.Println(string(stdout))
+	}
+
 	return nil
 }
